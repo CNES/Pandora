c3c9a45c9783bd6728f95045e2ddb4aa1db0840d
Marian Rassat <marian.rassat@csgroup.eu>
2025-01-15 17:43:32 +0100

    feat: easily change the census transform's data type, inverted cols and rows, renamed variables, commented census_transform

--- pandora/matching_cost/cpp/src/census.cpp
+++ pandora/matching_cost/cpp/src/census.cpp
@@ -1,59 +1,59 @@
 #include "census.hpp"
 #include <algorithm>
+#include <climits> // For CHAR_BIT
 #include <numeric>
 #include <cmath>
 #include <iostream>
 #include <bitset>
 #include <bit>
 
+#define CENSUS_TYPE char
+#define CENSUS_SINGLE_BIT (CENSUS_TYPE(1) << sizeof(CENSUS_TYPE)*CHAR_BIT - 1)
 
 namespace py = pybind11;
 
-void censuswprint(char* chs, int init, int nb) {
-    for (int i = 0; i < nb; i++) {
-        std::cout << std::bitset< 8 >( chs[init + i] );
-    }
-    std::cout << std::endl;
-}
-
-char* census_transform(py::array_t<float> img, int c_w, int c_h) {
+CENSUS_TYPE* census_transform(py::array_t<float> img, int census_width, int census_height) {
 
     auto r_img = img.unchecked<2>();
-    int n_cols = r_img.shape(0);
-    int n_rows = r_img.shape(1);
+    size_t n_rows = r_img.shape(0);
+    size_t n_cols = r_img.shape(1);
 
-    int c_half_w = c_w / 2;
-    int c_half_h = c_h / 2;
+    int c_half_w = census_width / 2;
+    int c_half_h = census_height / 2;
 
-    int nb_useful_bits = c_w * c_h;
-    int bits_per_elem = sizeof(char) * 8;
+    int nb_useful_bits = census_width * census_height;
+    int bits_per_elem = sizeof(CENSUS_TYPE) * CHAR_BIT;
     int nb_chars = nb_useful_bits / bits_per_elem + nb_useful_bits % bits_per_elem;
 
-    char* out = new char[n_rows*n_cols*nb_chars];
-    for (int i = 0; i < n_rows*n_cols*nb_chars; ++i) out[i] = 0;
+    CENSUS_TYPE* out = new CENSUS_TYPE[n_cols*n_rows*nb_chars];
+    for (int i = 0; i < n_cols*n_rows*nb_chars; ++i) out[i] = 0;
 
-    int s = 0;
-    
-    for (int x = c_half_h; x < n_cols - c_half_h; ++x) {
-        for (int y = c_half_w; y < n_rows - c_half_w; ++y) {
+    for (int x = c_half_h; x < n_rows - c_half_h; ++x) {
+        for (int y = c_half_w; y < n_cols - c_half_w; ++y) {
 
             // x,y -> all valid centers
             float val = r_img(x, y);
-            int out_pos = x*nb_chars*n_rows + y*nb_chars;
+            int out_pos = x*nb_chars*n_cols + y*nb_chars;
             int bit = 0;
             int chr = 0;
 
+            // for each image pixel (x,y), go through all the pixels in the window (wx, wy)
             for (int wx = x-c_half_h; wx < x+c_half_h+1; ++wx) {
                 for (int wy = y-c_half_w; wy < y+c_half_w+1; ++wy) {
                     
+                    // for the pixel (x,y), if the corresponding window pixel has a greater value,
+                    // set the window pixel's bit to 1
                     if (r_img(wx, wy) > val) {
-                        out[out_pos + chr] += 0x80 >> bit;
-                        s++;
-                    } 
+                        // CENSUS_SINGLE_BIT represents a CENSUS_TYPE with its highest bit set to 1
+                        out[out_pos + chr] += CENSUS_SINGLE_BIT >> bit;
+                    }
 
+                    // bit always goes up
                     bit++;
                     if (bit >= bits_per_elem) {
+                        // chr goes up once bit gets higher than the nb of bits in a CENSUS_TYPE
                         chr++;
+                        // reset bit
                         bit = 0;
                     }
 
@@ -79,37 +79,37 @@ py::array_t<float> compute_matching_costs(
 
     int min_disp = lround(rw_disps(0));
 
-    int n_cols = (int) rw_cv.shape(0);
-    int n_rows = (int) rw_cv.shape(1);
-    int n_disp = (int) rw_cv.shape(2);
+    size_t n_rows = rw_cv.shape(0);
+    size_t n_cols = rw_cv.shape(1);
+    size_t n_disp = rw_cv.shape(2);
 
     int c_half_w = census_width / 2;
     int c_half_h = census_height / 2;
 
     int nb_useful_bits = census_width * census_height;
-    int bits_per_elem = sizeof(char) * 8;
+    int bits_per_elem = sizeof(CENSUS_TYPE) * CHAR_BIT;
     int nb_chars = nb_useful_bits / bits_per_elem + nb_useful_bits % bits_per_elem;
 
-    char* census_img_left = census_transform(img_left, census_width, census_height);
+    CENSUS_TYPE* census_img_left = census_transform(img_left, census_width, census_height);
 
-    std::vector<char*> census_imgs_right = std::vector<char*>(); 
+    std::vector<CENSUS_TYPE*> census_imgs_right = std::vector<CENSUS_TYPE*>(); 
     for (py::handle handle_img : imgs_right) {
         py::array_t<float> img = py::cast<py::array>(handle_img);
         census_imgs_right.push_back( census_transform(img, census_width, census_height) );
     }
 
     int subpix = census_imgs_right.size();
-    char* right_img;
+    CENSUS_TYPE* right_img;
     
-    for (int col = c_half_h; col < n_cols-c_half_h; col++) {
-        for (int row = c_half_w; row < n_rows-c_half_w; row++) {
+    for (int row = c_half_h; row < n_rows-c_half_h; row++) {
+        for (int col = c_half_w; col < n_cols-c_half_w; col++) {
             
-            int left_pos = col*nb_chars*n_rows + row*nb_chars;
+            int left_pos = row*nb_chars*n_cols + col*nb_chars;
             for (int disp = 0; disp < n_disp; disp+=subpix) {
                 
-                int right_x = (row+disp/subpix+min_disp); // pixel
-                if (right_x < c_half_w || right_x >= n_rows-c_half_w) continue;
-                int right_pos = col*nb_chars*n_rows + right_x*nb_chars;
+                int right_x = (col+disp/subpix+min_disp); // pixel
+                if (right_x < c_half_w || right_x >= n_cols-c_half_w) continue;
+                int right_pos = row*nb_chars*n_cols + right_x*nb_chars;
 
                 for (int id_right = 0; (id_right < subpix) && (disp + id_right < n_disp); id_right++) {
                     
@@ -117,11 +117,11 @@ py::array_t<float> compute_matching_costs(
 
                     int weight = 0;
                     for (int chr = 0; chr < nb_chars; chr++) {
-                        char xr = right_img[right_pos+chr] ^ census_img_left[left_pos+chr];
-                        weight += std::bitset<8>(xr).count();
+                        CENSUS_TYPE xr = right_img[right_pos+chr] ^ census_img_left[left_pos+chr];
+                        weight += std::bitset<sizeof(CENSUS_TYPE)*CHAR_BIT>(xr).count();
                     }
 
-                    rw_cv(col, row, disp+id_right) = weight;
+                    rw_cv(row, col, disp+id_right) = weight;
                 }
 
             }
@@ -129,7 +129,7 @@ py::array_t<float> compute_matching_costs(
     }
 
     delete[] census_img_left;
-    for (char* img : census_imgs_right) {
+    for (CENSUS_TYPE* img : census_imgs_right) {
         delete[] img;
     }
 
